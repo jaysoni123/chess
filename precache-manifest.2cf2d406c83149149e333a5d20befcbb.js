fe606fb063c2e84583301a3dbd41fd54bf660e92
Dhananjay Choksi <soni.jay008@gmail.com>
2020-06-30 12:52:57 +0530

    Updates

---
+++ precache-manifest.2cf2d406c83149149e333a5d20befcbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424330a5025a736b3c5226a7a8f22326",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "d1a85068d200f88900cb",
+    "url": "/chess/static/css/main.35863d38.chunk.css"
+  },
+  {
+    "revision": "ba5f9d6da5c739db6d59",
+    "url": "/chess/static/js/2.cb184111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess/static/js/2.cb184111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1a85068d200f88900cb",
+    "url": "/chess/static/js/main.654fbabe.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  }
+]);
